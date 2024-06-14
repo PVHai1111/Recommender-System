c36f293b68a99896bffd1d142b46564ba7854e6b
Pham Viet Hai <pvhmain2k3@gmail.com>
2024-06-14 19:57:51 +0700

    init algo explain

--- Framework/EvaluationData.py
+++ Framework/EvaluationData.py
@@ -13,7 +13,7 @@ class EvaluationData:
         self.fullAntiTestSet = self.fullTrainSet.build_anti_testset()
         
         #Build a 75/25 train/test split for measuring accuracy
-        self.trainSet, self.testSet = train_test_split(data, test_size=.25, random_state=1)
+        self.trainSet, self.testSet = train_test_split(data, test_size=., random_state=1)
         
         #Build a "leave one out" train/test split for evaluating top-N recommenders
         #And build an anti-test-set for building predictions
